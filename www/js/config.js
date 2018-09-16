5ba357ecd51a9649b8bb0028f4c1cc4bf8218e71
srinivasan <sri9132@gmail.com>
2018-09-16 16:45:30 +0530

    Initial commit

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+var krms_config ={			
+	'ApiUrl':"http://kdboys.chiarro.com/mobileapp/api",		
+	'DialogDefaultTitle':"Kovai Delivery Boys",	
+	'pushNotificationSenderid':"465251440510",	
+	'facebookAppId':"882934511907337",
+	'APIHasKey':""
+};
+
